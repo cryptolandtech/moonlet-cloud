81ac32d6066af4e5bf32694b137fe7b8fd74c5a9
Krisboi Tibi <krisboi.tibi@gmail.com>
2019-04-12 02:17:45 +0300

    fix wpProxy function to forward post params

--- functions/wpProxy.js
+++ functions/wpProxy.js
@@ -1,7 +1,13 @@
 const functions = require('firebase-functions');
-
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer(); // See (†)
+
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+    if(req.method=="POST" && req.body){
+        proxyReq.write(req.body);
+        proxyReq.end();
+    }
+});
 
 const wpProxy = functions.https.onRequest(function(req, res) {
     proxy.web(req, res, { 
