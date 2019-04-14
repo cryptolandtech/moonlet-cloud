5744fb99b55df25c59cd4f167e63d97f7f14ddc1
Krisboi Tibi <krisboi.tibi@gmail.com>
2019-04-15 02:35:43 +0300

    website scrape script

---
+++ scripts/scrape-website.js
@@ -0,0 +1,22 @@
+const scrape = require('website-scraper');
+var evilDns = require('evil-dns');
+
+// String match
+evilDns.add('moonlet.xyz', '212.146.84.81');
+
+
+const options = {
+  urls: [
+    'https://moonlet.xyz/',
+    {url: 'https://moonlet.xyz/404.html', filename: '404.html'}
+  ],
+  directory: './website',
+  recursive: true,
+  urlFilter: function(url){
+    return url.indexOf('https://moonlet.xyz') === 0 || url.indexOf('http://moonlet.xyz') === 0;
+  }
+};
+
+scrape(options).then((result) => {
+    console.log('Done!');
+});
