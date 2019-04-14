719664b46bdde048d314750c34a053ae152aad12
Krisboi Tibi <krisboi.tibi@gmail.com>
2019-04-15 01:10:35 +0300

    fix wp proxy function

--- functions/wpProxy.js
+++ functions/wpProxy.js
@@ -4,12 +4,18 @@ const proxy = httpProxy.createProxyServer(); // See (†)
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
     if(req.method=="POST" && req.body){
-        proxyReq.write(req.body);
+        let body = req.body;
+        if (typeof req.body === "object") {
+            body = JSON.stringify(req.body);
+            console.log("POST", req.url, body);
+        }
+        proxyReq.write(body);
         proxyReq.end();
     }
 });
 
 const wpProxy = functions.https.onRequest(function(req, res) {
+    //console.log(req.url);
     proxy.web(req, res, { 
         target: 'https://212.146.84.81', 
         headers: {
