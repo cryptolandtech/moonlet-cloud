27bd9528888472049aeb70771d4943f0cb6e6714
Krisboi Tibi <krisboi.tibi@gmail.com>
2019-05-12 18:18:18 +0300

    fix ledger bridge

--- src/ledger-bridge/preact.config.js
+++ src/ledger-bridge/preact.config.js
@@ -1,5 +1,21 @@
 import asyncPlugin from 'preact-cli-plugin-fast-async';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import { resolve } from 'path';
 
 export default (config) => {
     asyncPlugin(config);
+    config.output.publicPath = '/ledger-bridge/'
+
+    config.node.process = 'mock';
+    config.node.Buffer = true;
+
+    // // overwrite manifest.json
+    // config.plugins.unshift(
+    //     new CopyWebpackPlugin([
+    //         {
+    //             from: resolve(process.cwd(), 'src', 'manifest.json'),
+    //             to: 'manifest.json'
+    //         }
+    //     ])
+    // );
 }
