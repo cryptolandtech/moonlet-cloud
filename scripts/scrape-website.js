81dd4c4a657ea292b56f37788bd538b7083bea21
Krisboi Tibi <krisboi.tibi@gmail.com>
2019-05-22 02:18:23 +0300

    add thankyou pages to scraping script

--- scripts/scrape-website.js
+++ scripts/scrape-website.js
@@ -9,7 +9,11 @@ scrapper({
     urls: [
         'https://moonlet.xyz',
         'https://moonlet.xyz/404.html',
-        'https://moonlet.xyz/favicon.ico'
+        'https://moonlet.xyz/favicon.ico',
+        
+        'https://moonlet.xyz/thank-you/',
+        'https://moonlet.xyz/thank-you-update/',
+        'https://moonlet.xyz/thank-you-delete/'
     ],
     dest: './hosting/moonlet.xyz',
     urlFilter: (url) => (url || "").startsWith('https://moonlet.xyz')
